7a055e55b6bbb41aea4c5af9b58e70572123a722
Miron Maksymiuk <miron.maksymiuk@globallogic.com>
2023-06-14 16:36:03 +0200

    Added repository layer to the bookstore backend project.

---
+++ src/main/java/pl/kazanik/basicfullstack/repository/BookRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package pl.kazanik.basicfullstack.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.kazanik.basicfullstack.dto.BookDto;
+
+/**
+ *
+ * @author miron.maksymiuk
+ */
+@Repository
+public interface BookRepository extends JpaRepository<BookDto, Long>{
+    
+}
